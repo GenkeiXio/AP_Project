caed9fcbbf550cd023573ccdbff1ae082bedde16
v1-Siege <christophersayson665@gmail.com>
2026-03-27 10:57:33 +0800

    main map with pins

--- routes/web.php
+++ routes/web.php
@@ -88,3 +88,7 @@ Route::get('/module', function () {
 Route::get('/pre-test/module-2', function () {
     return view('Students.Pre-Test.module2');
 })->name('pretest.module2');
+
+Route::get('/demo-map', function () {
+    return view('Students.Games.mainmap'); // This matches the name 'mainmap.blade.php'
+})->name('student.map'); // This is the 'nickname' we use in the href
