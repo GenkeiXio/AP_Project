e2957ce447ec42b8c3b84746b6121f743218bf86
Maykel-Lunch <mnl2022-1999-95982@bicol-u.edu.ph>
2026-06-19 10:37:27 +0800

    add: favicon

--- resources/views/Students/studentslayout.blade.php
+++ resources/views/Students/studentslayout.blade.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&family=Baloo+2:wght@600;700;800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="pictures/landingpage_titleimage.png">
     <style>
         *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
         body {
